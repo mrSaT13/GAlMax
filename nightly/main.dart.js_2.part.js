385a8216273e702614be2f1494c36b9be67c5e2a
mrSaT13 <mrSaT13@users.noreply.github.com>
2026-09-15 11:26:41 +0000

    deploy: dd97c75fbf9b49fa0345d17e17d8410477423d7c

---
+++ nightly/main.dart.js_2.part.js
@@ -0,0 +1,10 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aN:function aN(){}}
+B=c[0]
+A=a.updateHolder(c[58],A)
+A.aN.prototype={
+C(d,e){return this.gbc(this).$1(e)}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.aN,B.A)})()};
+(a=>{a["UOv8Wh6dWU+rrD67XgXOxPQLT4M="]=a.current})($__dart_deferred_initializers__);
+//# sourceMappingURL=main.dart.js_2.part.js.map
